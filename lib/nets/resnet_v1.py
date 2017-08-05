956df39e892d35a661046c1875ce2ad25f0d8f7d
Noah Su <noahsuhk@gmail.com>
2017-08-05 20:53:22 +0800

    minor changes

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -229,9 +229,9 @@ class resnetv1(Network):
       #                              scope=self._resnet_scope)
 
       # this line is only for test, delete it!
-      print('shape of pool5: {}'.format(tf.shape(pool5)))
+      print('shape of pool5: {}'.format(pool5.get_shape()))
       fc7 = slim.fully_connected(pool5, 1024)
-      print('shape of fc7: {}'.format(tf.shape(fc7)))
+      print('shape of fc7: {}'.format(fc7.get_shape()))
 
     with tf.variable_scope(self._resnet_scope, self._resnet_scope):
       # Average pooling done by reduce_mean
