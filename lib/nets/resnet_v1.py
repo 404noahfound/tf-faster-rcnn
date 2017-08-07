90e4ecaeecb6db11a63ec8917834524ead54d56d
Noah Su <noahsuhk@gmail.com>
2017-08-07 15:31:42 +0800

    minor changes

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -235,6 +235,7 @@ class resnetv1(Network):
     with tf.variable_scope(self._resnet_scope, self._resnet_scope):
       # Average pooling done by reduce_mean
       fc7 = tf.reduce_mean(fc7, axis=[1, 2])
+      print('shape of fc7: {}'.format(fc7.get_shape()))
       cls_score = slim.fully_connected(fc7, self._num_classes, weights_initializer=initializer,
                                        trainable=is_training, activation_fn=None, scope='cls_score')
       cls_pred = tf.argmax(cls_score, axis=1, name="cls_pred")
