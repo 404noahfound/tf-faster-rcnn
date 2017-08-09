1e96d6910963baa1cb0751d7acbb0c69d8771bfe
Noah Su <noahsuhk@gmail.com>
2017-08-09 11:30:24 +0800

    minor changes

--- lib/nets/fpn.py
+++ lib/nets/fpn.py
@@ -100,10 +100,8 @@ class FeaturePyramidNetwork():
 
   def merger(self, name_list, stage_outputs, merge_outputs):
     for name in name_list:
-      print(name_list[name])
-      merge_outputs[name] = self.merger_for(stage_outputs[name],
-        merge_outputs[name], axis=name_list[name])
+      merge_outputs[name] = self.merger_for(stage_outputs[name], name_list[name])
     return merge_outputs
 
-  def merger_for(self, stage_outputs, axis=1):
+  def merger_for(self, stage_outputs, axis):
     return tf.concat(values=stage_outputs.values(), axis=axis)
