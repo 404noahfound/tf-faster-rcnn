88698a96acf506f83354ae2d3209655a31495fe2
Noah Su <noahsuhk@gmail.com>
2017-08-09 11:32:48 +0800

    minor changes

--- experiments/scripts/train_fpn.sh
+++ experiments/scripts/train_fpn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-GPU_ID='1,2,3'
+GPU_ID='0,1'
 CUDA_VISIBLE_DEVICES=${GPU_ID}
 ./experiments/scripts/train_faster_rcnn.sh ${GPU_ID} coco_minival res50 fpn
--- lib/nets/rpn_fpn.py
+++ lib/nets/rpn_fpn.py
@@ -114,7 +114,6 @@ class RPN_FPN(FeaturePyramidNetwork):
         self._stage_outputs[output_group],
         self._merge_outputs[output_group]
       )
-      print('stage content of {}:\n{}'.format(output_group, self._stage_outputs[output_group]))
       print('merge content of {}:\n{}'.format(output_group, self._merge_outputs[output_group]))
     base_net._anchor_targets = self._merge_outputs['anchor_targets']
     base_net._proposal_targets = self._merge_outputs['proposal_targets']
