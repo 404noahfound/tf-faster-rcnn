657728d17a812e8f2843a5b42c0eb46e229a94e5
Noah Su <noahsuhk@gmail.com>
2017-08-09 09:00:32 +0800

    update trian-fpn

--- experiments/scripts/train_fpn.sh
+++ experiments/scripts/train_fpn.sh
@@ -1,6 +1,92 @@
-#!/bin/bash
-GPU_ID='2'
-CUDA_VISIBLE_DEVICES=${GPU_ID}
-./experiments/scripts/train_faster_rcnn.sh ${GPU_ID} coco res50 \
-  --cfg fpn.yml \
-  --tag rpn_fpn
+  #!/bin/bash
+
+  set -x
+  set -e
+
+  export PYTHONUNBUFFERED="True"
+
+  GPU_ID='1,2,3'
+  DATASET='coco_minival'
+  NET='res50'
+  CFG='fpn.yml'
+  TAG='fpn'
+
+  EXTRA_ARGS_SLUG=TAG
+
+
+  case ${DATASET} in
+    pascal_voc)
+      TRAIN_IMDB="voc_2007_trainval"
+      TEST_IMDB="voc_2007_test"
+      STEPSIZE=50000
+      ITERS=70000
+      ANCHORS="[8,16,32]"
+      RATIOS="[0.5,1,2]"
+      ;;
+    pascal_voc_0712)
+      TRAIN_IMDB="voc_2007_trainval+voc_2012_trainval"
+      TEST_IMDB="voc_2007_test"
+      STEPSIZE=80000
+      ITERS=110000
+      ANCHORS="[8,16,32]"
+      RATIOS="[0.5,1,2]"
+      ;;
+    coco)
+      TRAIN_IMDB="coco_2014_train+coco_2014_valminusminival"
+      TEST_IMDB="coco_2014_minival"
+      STEPSIZE=350000
+      ITERS=490000
+      ANCHORS="[4,8,16,32]"
+      RATIOS="[0.5,1,2]"
+      ;;
+    coco_minival)
+      TRAIN_IMDB="coco_2014_minival"
+      TEST_IMDB="coco_2014_minival"
+      STEPSIZE=350000
+      ITERS=490000
+      ANCHORS="[4,8,16,32]"
+      RATIOS="[0.5,1,2]"
+      ;;
+    *)
+      echo "No dataset given"
+      exit
+      ;;
+  esac
+
+  LOG="experiments/logs/${NET}_${TRAIN_IMDB}_${EXTRA_ARGS_SLUG}_${NET}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+  exec &> >(tee -a "$LOG")
+  echo Logging output to "$LOG"
+
+  set +x
+  if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
+      NET_FINAL=output/${NET}/${TRAIN_IMDB}/${EXTRA_ARGS_SLUG}/${NET}_faster_rcnn_iter_${ITERS}.ckpt
+  else
+      NET_FINAL=output/${NET}/${TRAIN_IMDB}/default/${NET}_faster_rcnn_iter_${ITERS}.ckpt
+  fi
+  set -x
+
+  if [ ! -f ${NET_FINAL}.index ]; then
+      if [[ ! -z  ${EXTRA_ARGS_SLUG}  ]]; then
+          CUDA_VISIBLE_DEVICES=${GPU_ID} time python ./tools/trainval_net.py \
+              --weight data/imagenet_weights/${NET}.ckpt \
+              --imdb ${TRAIN_IMDB} \
+              --imdbval ${TEST_IMDB} \
+              --iters ${ITERS} \
+              --cfg experiments/cfgs/${CFG}.yml \
+              --tag ${EXTRA_ARGS_SLUG} \
+              --net ${NET} \
+              --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS}
+      else
+          CUDA_VISIBLE_DEVICES=${GPU_ID} time python ./tools/trainval_net.py \
+              --weight data/imagenet_weights/${NET}.ckpt \
+              --imdb ${TRAIN_IMDB} \
+              --imdbval ${TEST_IMDB} \
+              --iters ${ITERS} \
+              --cfg experiments/cfgs/${CFG}.yml \
+              --net ${NET} \
+              --set ANCHOR_SCALES ${ANCHORS} ANCHOR_RATIOS ${RATIOS} TRAIN.STEPSIZE ${STEPSIZE} ${EXTRA_ARGS}
+      fi
+  fi
+
+  ./experiments/scripts/test_faster_rcnn.sh $@
+  mv LOG LOG".done"
