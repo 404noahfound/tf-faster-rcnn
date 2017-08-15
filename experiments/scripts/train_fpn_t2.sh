64ca58579b61ef1a8d878f3b789cab9f9cf39f74
Noah Su <noahsuhk@gmail.com>
2017-08-15 16:57:51 +0800

    add multi scale anchor in sh

--- experiments/scripts/train_fpn_t2.sh
+++ experiments/scripts/train_fpn_t2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 GPU_ID='6,7'
 CUDA_VISIBLE_DEVICES=${GPU_ID}
-./experiments/scripts/train_faster_rcnn.sh ${GPU_ID} coco_fpn res50 fpn test2
+./experiments/scripts/train_faster_rcnn.sh ${GPU_ID} coco_fpn res50 fpn multi_scale_anchor
